a6dc84dd62800758c2a12a71145ffb6c396ed8de
LexoAtiyota <50679572+LexoAtiyota@users.noreply.github.com>
2021-11-10 20:45:49 -0600

    Add files via upload

---
+++ 10_17.py
@@ -0,0 +1,10 @@
+user_input = input().split()
+
+for i in range(len(user_input)):
+    user_input[i] = int(user_input[i])
+
+user_input.sort()
+
+for num in user_input:
+    if num >= 0:
+        print(num, end=' ')
